43ce6697292716fa68b67f22d27e388c1dc09b67
Pedro Pupo <ppalves10@gmail.com>
2021-07-25 22:23:51 -0300

    change certificate email

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,1 +1,1 @@
-sudo certbot -n -d pronto3d.com.br,www.pronto3d.com.br --nginx --agree-tos --no-eff-email --email ppalves10@gmail.com
+sudo certbot -n -d pronto3d.com.br,www.pronto3d.com.br --nginx --agree-tos --no-eff-email --email pronto3d@gmail.com
